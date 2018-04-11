f3fa59610fb1ac0157771e29f1f1a64413e82a94
Alexander Myshov <simplex.light@gmail.com>
2018-04-12 00:10:38 +0700

    Remove redundant reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -42,21 +42,8 @@ const todos = (state = [], action) => {
     }
 }
 
-const visibilityFilter = (
-    state = 'SHOW_ALL',
-    action
-) => {
-    switch (action.type) {
-        case 'SET_VISIBILITY_FILTER':
-            return action.filter;
-        default:
-            return state;
-    }
-}
-
 const rootReducer = combineReducers({
     todos,
-    visibilityFilter
 });
 
 const testAddTodo = () => {
