b23fb5ea4ac5e76933d06757b7a8033c0e79080b
Alexander Myshov <simplex.light@gmail.com>
2018-04-12 23:30:53 +0700

    Fetch data in lifecycle hooks

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,18 +1,46 @@
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router-dom';
 
+import {fetchTodos} from '../api';
 import {toggleTodo} from '../actions';
 import TodoList from '../components/TodoList';
 import {getVisibleTodos} from '../reducers';
 
-const mapStateToProps = (state, {match}) => ({
-    todos: getVisibleTodos(
-        state,
-        match.params.filter
-    )
-});
 
-export default withRouter(connect(
+class VisibleTodoList extends Component {
+    componentDidMount() {
+        const {filter} = this.props;
+        fetchTodos(filter).then((todos) => {
+            console.log(filter, todos);
+        });
+    }
+
+    componentDidUpdate(prevProps) {
+        const {filter} = this.props;
+        if (prevProps.filter !== filter) {
+            fetchTodos(filter).then((todos) => {
+                console.log(filter, todos);
+            });
+        }
+    }
+
+    render() {
+        return <TodoList {...this.props} />
+    }
+}
+
+const mapStateToProps = (state, {match}) => {
+    const filter = match.params.filter || 'all';
+    return {
+        todos: getVisibleTodos(state, filter),
+        filter,
+    };
+};
+
+VisibleTodoList = withRouter(connect(
     mapStateToProps,
     {onTodoClick: toggleTodo}
-)(TodoList));
+)(VisibleTodoList));
+
+export default VisibleTodoList;
