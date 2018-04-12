3e568fd1baa5f86ddca21edcaeef3abd54077def
Alexander Myshov <simplex.light@gmail.com>
2018-04-12 17:12:28 +0700

    Move todos reducer into separate file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,108 +1,10 @@
 import {combineReducers} from 'redux';
-import expect from 'expect';
-import deepFreeze from 'deep-freeze';
 
-const todo = (state, action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            };
+import {todos} from './todos';
 
-        case 'TOGGLE_TODO':
-            if (state.id !== action.id) {
-                return state;
-            }
-
-            return {
-                ...state,
-                completed: !state.completed
-            };
-
-        default:
-            return state;
-    }
-}
-
-const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ];
-
-        case 'TOGGLE_TODO':
-            return state.map(t => todo(t, action));
-
-        default:
-            return state;
-    }
-}
 
 const rootReducer = combineReducers({
     todos,
 });
-
-const testAddTodo = () => {
-    const stateBefore = [];
-    const stateAfter = [{
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-    }];
-    const action = {
-        type: 'ADD_TODO',
-        id: 0,
-        text: 'Learn Redux'
-    };
-
-    deepFreeze(stateBefore);
-    deepFreeze(action);
-
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
-}
-
-const testToggleTodo = () => {
-    const stateBefore = [{
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-    }, {
-        id: 1,
-        text: 'Go shopping',
-        completed: false
-    }];
-
-    const action = {
-        type: 'TOGGLE_TODO',
-        id:1
-    };
-
-    const stateAfter = [{
-        id: 0,
-        text: 'Learn Redux',
-        completed: false
-    }, {
-        id: 1,
-        text: 'Go shopping',
-        completed: true
-    }];
-
-    deepFreeze(stateBefore);
-    deepFreeze(action);
-
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
-}
-
-testAddTodo();
-testToggleTodo();
-console.log('test passed');
 
 export default rootReducer;
