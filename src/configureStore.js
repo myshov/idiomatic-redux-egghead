fbe4d98bdf1b181e8ea01cde05d0a9efa6854e99
Alexander Myshov <simplex.light@gmail.com>
2018-04-13 01:40:03 +0700

    Use libraries from npm for middlewares

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,52 +1,21 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import promise from 'redux-promise';
+import {createLogger} from 'redux-logger';
 
 import rootReducer from './reducers';
 
 
-const logger = (store) => {
-    return (next) => {
-        if (!console.group) {
-            return next;
-        }
-
-        return (action) => {
-            console.group(action.type);
-            console.log('%c prev state', 'color: gray', store.getState());
-            console.log('%c action', 'color: blue', action);
-            const returnValue = next(action);
-            console.log('%c next state', 'color: green', store.getState());
-            console.groupEnd(action.type);
-            return returnValue;
-        }
-    }
-}
-
-const promiseMiddleware = (store) => (next) => (action) => {
-    if (action instanceof Promise) {
-        return action.then(next);
-    } else {
-        return next(action);
-    }
-};
-
-const wrapDispatchWithMiddlewares = (store, middlewares) => {
-    middlewares.slice().reverse().forEach((middleware) => {
-        store.dispatch = middleware(store)(store.dispatch);
-    });
-}
-
 const configureStore = () => {
+    const middlewares = [promise];
+    if (process.env.NODE_ENV !== 'production') {
+        middlewares.push(createLogger());
+    }
+
     const todoStore = createStore(
         rootReducer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        applyMiddleware(...middlewares)
+        /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
     );
-    const middlewares = [promiseMiddleware];
-
-    if (process.env.NODE_ENV !== 'production') {
-        middlewares.push(logger);
-    }
-
-    wrapDispatchWithMiddlewares(todoStore, middlewares);
 
     return todoStore;
 }
