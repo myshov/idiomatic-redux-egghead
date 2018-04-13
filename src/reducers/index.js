a5786b2dfca7622369850af3e8565cfbf60627a4
Alexander Myshov <simplex.light@gmail.com>
2018-04-13 19:45:27 +0700

    Refactor reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,23 @@
 import {combineReducers} from 'redux';
 
-import todos, * as fromTodos from './todos';
+import byId, * as fromById from './byId';
+import createList, * as fromList from './createList';
 
 
-const rootReducer = combineReducers({
-    todos,
+const listByFilter = combineReducers({
+    all: createList('all'),
+    active: createList('active'),
+    completed: createList('completed'),
 });
 
-export default rootReducer;
+const todos = combineReducers({
+    byId,
+    listByFilter,
+});
 
-export const getVisibleTodos = (state, filter) =>
-    fromTodos.getVisibleTodos(state.todos, filter);
+export default todos;
+
+export const getVisibleTodos = (state, filter) => {
+    const ids = fromList.getIds(state.listByFilter[filter]);
+    return ids.map(id => fromById.getTodo(state.byId, id));
+};
